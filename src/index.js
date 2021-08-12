4f25839f1d2b898925f53cb57928b49d283e648d
adrianammt <amorat@gmx.de>
2021-08-12 21:14:30 +0200

    Change color names and color.css link to index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
-import "./Colors.css";
+import "./colors.css";
 
 ReactDOM.render(
   <React.StrictMode>
