53108de3c6186f7ba499d301cfe3d39cbbb7d258
adrianammt <amorat@gmx.de>
2021-08-13 17:45:26 +0200

    Refactor code to make ClassCard a component that receives props from MyList Page

--- src/pages/MyList.js
+++ src/pages/MyList.js
@@ -1,4 +1,26 @@
 import ClassCard from "../components/ClassCard";
+import { useEffect, useState } from "react";
+
 export default function MyList() {
-  return <ClassCard />;
+  const [savedClasses, setSavedClasses] = useState([]);
+
+  useEffect(() => {
+    const savedClasses = JSON.parse(localStorage.getItem("classList")) || [];
+    setSavedClasses(savedClasses);
+  }, []);
+
+  return (
+    <div className="ClassCard__wrapper">
+      {savedClasses.map((savedClass) => {
+        return (
+          <ClassCard
+            key={savedClass.id}
+            name={savedClass.name}
+            duration={savedClass.duration}
+            intervalTime={savedClass.intervalTime}
+          />
+        );
+      })}
+    </div>
+  );
 }
