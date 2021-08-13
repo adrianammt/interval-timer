8346a697a0f236f50ac63a0bdeb1b994e9c8d2b1
adrianammt <amorat@gmx.de>
2021-08-13 15:14:05 +0200

    Change c to C in colors.css

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
-import "./colors.css";
+import "./Colors.css";
 
 ReactDOM.render(
   <React.StrictMode>
