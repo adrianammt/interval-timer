ee3ed6945317122639bf617be2f87c101658a978
adrianammt <amorat@gmx.de>
2021-08-16 21:55:47 +0200

    Create onClick handler and passed the id to filter removed classes, added id to the ClassCard props, added useEffect to set new filtered array into local storage

--- src/pages/MyList.js
+++ src/pages/MyList.js
@@ -1,13 +1,24 @@
-import ClassCard from "../components/ClassCard";
+import ClassCard from "../components/ClassCard/ClassCard";
 import { useEffect, useState } from "react";
 
 export default function MyList() {
   const [savedClasses, setSavedClasses] = useState([]);
 
+  function handleRemoveClass(id) {
+    const newSavedClasses = savedClasses.filter(
+      (savedClass) => savedClass.id !== id
+    );
+    setSavedClasses(newSavedClasses);
+  }
+
   useEffect(() => {
     const savedClasses = JSON.parse(localStorage.getItem("classList")) || [];
     setSavedClasses(savedClasses);
   }, []);
+
+  useEffect(() => {
+    localStorage.setItem("classList", JSON.stringify(savedClasses));
+  }, [savedClasses]);
 
   return (
     <div className="ClassCard__wrapper">
@@ -15,9 +26,11 @@ export default function MyList() {
         return (
           <ClassCard
             key={savedClass.id}
+            id={savedClass.id}
             name={savedClass.name}
             duration={savedClass.duration}
             intervalTime={savedClass.intervalTime}
+            handleRemoveClass={handleRemoveClass}
           />
         );
       })}
