1bdff55eb80e93a28594af3ebc15b5d998ac799c
adrianammt <amorat@gmx.de>
2021-08-17 21:49:23 +0200

    Control Icons and Favourite Icon are changing on click, added some css

--- src/components/PlayClassCard/PlayClassCard.stories.js
+++ src/components/PlayClassCard/PlayClassCard.stories.js
@@ -11,11 +11,11 @@ const Template = (args) => <PlayClassCard {...args} />;
 export const Default = Template.bind({});
 Default.args = {
   id: 1,
-  name: "Good morning Sunshine",
+  name: "Rise and shine",
   duration: "00:30",
-  intervalTime: "00:01",
+  intervaltime: "00:01",
   prepTime: "10 sec",
-  isPlayed: false,
+  isPlaying: false,
   isStopped: false,
   isPaused: false,
   isToggled: false,
