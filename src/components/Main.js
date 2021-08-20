af8ef2263be95132543621df88c01cbf75c2a37f
adrianammt <amorat@gmx.de>
2021-08-20 15:53:22 +0200

    Create a new Route for displayinf PlayCard and redirect home path to MyList page

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,7 @@ import { Switch, Route, Redirect } from "react-router-dom";
 import Create from "../pages/Create";
 import MyList from "../pages/MyList";
 import Favourites from "../pages/Favourites";
+import PlayClass from "../pages/PlayClass";
 import "./Main.css";
 import { ToastContainer } from "react-toast";
 
@@ -10,6 +11,9 @@ export default function Main() {
     <main className="Content">
       <ToastContainer delay={3000} position={"top-center"} />
       <Switch>
+        <Route path="/myList/:id">
+          <PlayClass />
+        </Route>
         <Route path="/create">
           <Create />
         </Route>
@@ -20,7 +24,7 @@ export default function Main() {
           <Favourites />
         </Route>
         <Route path="/">
-          <Redirect to="/create" />
+          <Redirect to="/myList" />
         </Route>
       </Switch>
     </main>
