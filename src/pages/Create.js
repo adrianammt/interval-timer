53ffb80d5efeae9311784bc7bc758bf59121af69
adrianammt <amorat@gmx.de>
2021-08-29 14:38:43 +0200

    Filter classToEdit and passed down functions into the form

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,5 +1,23 @@
 import SettingsForm from "../components/SettingsForm";
 
-export default function Create({ onSaveFormInput }) {
-  return <SettingsForm onSaveFormInput={onSaveFormInput} />;
+export default function Create({
+  handleSaveFormInput,
+  handleEditClass,
+  listOfClasses,
+  classIdToEdit,
+}) {
+  const classToEdit = listOfClasses.filter(
+    (savedClass) => savedClass.id === classIdToEdit
+  );
+  console.log(listOfClasses);
+  console.log(classToEdit);
+  console.log(classIdToEdit);
+
+  return (
+    <SettingsForm
+      classToEdit={classToEdit[0]}
+      handleSaveFormInput={handleSaveFormInput}
+      handleEditClass={handleEditClass}
+    />
+  );
 }
