22c96644129de8f1ff008440f0e0bb965a43fd9f
adrianammt <amorat@gmx.de>
2021-08-29 14:28:43 +0200

    Renamed passed function to handleSaveFormInput. Passed handleEditClass. Passed new functions to Create and PlayClass pages.

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,20 +7,29 @@ import "./Main.css";
 import { ToastContainer } from "react-toast";
 
 export default function Main({
-  onSaveFormInput,
+  handleSaveFormInput,
   listOfClasses,
   onRemoveClassClick,
   toggleFavourite,
+  handleEditClass,
 }) {
   return (
     <main className="Content">
       <ToastContainer delay={2000} position={"top-center"} />
       <Switch>
         <Route path="/myList/:id">
-          <PlayClass data={listOfClasses} toggleFavourite={toggleFavourite} />
+          <PlayClass
+            listOfClasses={listOfClasses}
+            toggleFavourite={toggleFavourite}
+            handleEditClass={handleEditClass}
+          />
         </Route>
         <Route path="/create">
-          <Create onSaveFormInput={onSaveFormInput} />
+          <Create
+            handleSaveFormInput={handleSaveFormInput}
+            listOfClasses={listOfClasses}
+            handleEditClass={handleEditClass}
+          />
         </Route>
         <Route path="/myList">
           <MyList
