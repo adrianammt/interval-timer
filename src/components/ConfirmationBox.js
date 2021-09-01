5cb472125069113b16c098dd151556e7ed68c84a
adrianammt <amorat@gmx.de>
2021-09-01 12:09:13 +0200

    Created a new component for the delete confirmation.

---
+++ src/components/ConfirmationBox.js
@@ -0,0 +1,31 @@
+import "./ConfirmationBox.css";
+
+export default function ConfirmationBox({ onCancel, onConfirm, isHide, name }) {
+  return (
+    <>
+      <div className={isHide ? "ConfirmationBox" : "ConfirmationBox-show"}>
+        <p className="ConfirmationBox__title">Do you want to delete {name}?</p>
+        <div className="Buttons-wrapper">
+          <button
+            onClick={(e) => {
+              e.stopPropagation();
+              onCancel();
+            }}
+            className="Buttons"
+          >
+            No
+          </button>
+          <button
+            onClick={(e) => {
+              e.stopPropagation();
+              onConfirm();
+            }}
+            className="Buttons"
+          >
+            Yes
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
