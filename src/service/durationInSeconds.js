43a239c0d43c9244b5a67c6db70bcaf87c0281e9
adrianammt <amorat@gmx.de>
2021-09-04 20:51:06 +0200

    Transition in message box is quicker. Deleted old bg image.

--- src/service/durationInSeconds.js
+++
@@ -1,7 +0,0 @@
-export default function getTimeInSeconds(timeInput) {
-  const hours = timeInput.split(":")[0];
-  const minutes = timeInput.split(":")[1];
-  const timeInSeconds = Number(hours) * 3600 + Number(minutes) * 60;
-
-  return timeInSeconds;
-}
