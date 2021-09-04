e0ff747dd7fa6255071f7841ce20a42786b753dd
adrianammt <amorat@gmx.de>
2021-09-04 14:41:43 +0200

    Renamed submit button to Done, added useHistory to go back to the class being played.

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,23 +1,25 @@
 import SettingsForm from "../components/SettingsForm";
 import { useParams } from "react-router-dom";
 import useClassList from "../hooks/useClassList";
-
+import { useHistory } from "react-router";
 export default function EditSettings() {
   const { classList, updateClass } = useClassList();
   const { id } = useParams();
+
+  const history = useHistory();
 
   const classToEdit = classList.find((savedClass) => savedClass.id === id);
 
   function handleUpdateClassSettings(updatedClass) {
     updateClass(id, updatedClass);
+    history.goBack();
   }
 
   return (
     <SettingsForm
       initialClassInput={classToEdit}
       id={id}
-      buttonName="Update
-  Class"
+      buttonName="Done"
       onSubmit={handleUpdateClassSettings}
     />
   );
